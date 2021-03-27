cbce79738c5198171d1622c1ed502d64fd1d60fb
Rodolfo Mayora <rodolfomayora96@gmail.com>
2021-03-27 18:32:37 -0400

    BLP-0: Project Configuration

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react';
+import Head from 'next/head'
+
+const Home: FC = () => (
+  <div>
+    <Head>
+      <title>Create Next App</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <h1>hola</h1>
+  </div>
+);
+
+export default Home;
