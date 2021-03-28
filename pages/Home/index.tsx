6752cb04f6e08b1a8bf26bd8c620dd19a94aff3d
Rodolfo Mayora <rodolfomayora96@gmail.com>
2021-03-28 10:29:35 -0400

    BLP-2: Create Layout and Container

--- pages/Home/index.tsx
+++ pages/Home/index.tsx
@@ -1,21 +1,17 @@
 import React, { FC } from 'react';
-import Head from 'next/head'
+import { Layout, Container } from '../../components';
 import styles from './style.module.scss';
 
 const Home: FC = () => (
-  <div className={styles.Home}>
-    <Head>
-      <title>Home | Blogr Landing Page</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta name="description" content="page description" />
-      <meta name="keywords" content="blogr, blog, blogs, post, posts editor, content, management" />
-      <link rel="icon" href="/favicon.png" />
-    </Head>
-
-    <section>
-      <h1>HOLA</h1>
-    </section>
-  </div>
+  <Layout pageTitle='Home'>
+    <main className={styles.Home}>
+      <Container>
+        <section>
+          <h1>HOLA Layout</h1>
+        </section>
+      </Container>
+    </main>
+  </Layout>
 );
 
 export default Home;
