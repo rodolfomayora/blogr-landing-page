0304e2aa8c430e9d74ab272b9306dcd236219c61
Rodolfo Mayora <rodolfomayora96@gmail.com>
2021-03-29 19:45:46 -0400

    ELP-10: MainSection Desktop

--- pages/Home/index.tsx
+++ pages/Home/index.tsx
@@ -28,11 +28,55 @@ const Home: FC = () => (
         />
       </section>
       
-      <section>
+      {/* Feature Section 1 */}
+      <section className={style.featureSection1}>
+        <Container>
+          <h2 className={style.titleSection}>Designed for the future</h2>
+          <div>
+            <img className={style.sectionImage}
+              src="/images/svg/illustration-editor-mobile.svg"
+              alt="Illustation Editor Mobile"
+            />
+          </div>
+          <section className={style.feature}>
+            <h3 className={style.featureTitle}>Introducing an extensible editor</h3>
+            <p className={style.description}>Blogr features an exceedingly intuitive interface which lets you focus on one thing: creating content. The editor supports management of multiple blogs and allows easy manipulation of embeds such as images, videos, and Markdown. Extensibility with plugins and themes provide easy ways to add functionality or change the looks of a blog.</p>
+          </section>
+
+          <section className={style.feature}>
+            <h3 className={style.featureTitle}>Robust content management</h3>
+            <p className={style.description}>Flexible content management enables users to easily move through posts. Increase the usability of your blog by adding customized categories, sections, format, or flow. With this functionality, you're in full control.</p>
+          </section>
+        </Container>
+
+        <img className={style.sectionImageDesktop}
+            src="/images/svg/illustration-editor-desktop.svg"
+            alt="Illustation Editor Desktop"
+          />
       </section>
         
+      {/* BannerSection */}
+      <section>
+        <Container>
+          <h2>State of the Art Infraestructure</h2>
+          <p>With reliability and speed in mind, worldwide data centers provide the backbone for ultra-fast connectivity. This ensures your site will load instantly, no matter where your readers are, keeping your site competitive.</p>
+        </Container>
+      </section>
 
-      
+      {/* Feature Section 2 */}
+      <div className={style.featureSection2}>
+        <Container>
+          <section>
+            <h3>Free, open, simple</h3>
+            <p>Blogr is a free and open source application backed by a large community of helpful developers, It supports features such as code syntax highlighting, RSS feeds, social media integration, third-party commenting tools, and works seamlessly with Google Analytics, The architecture is clean and is relatively easy to learn.</p>
+          </section>
+
+          <section>
+            <h3>Powerful tooling</h3>
+            <p>Batteries Included. We built a simple and straightfordward CLI tool that makes customization and deployment a breeze, but capable of producing even the most complicated sites.</p>
+          </section>
+        </Container>
+      </div>
     </main>
   </Layout>
 );
