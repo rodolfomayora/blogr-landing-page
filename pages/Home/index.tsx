6ecd65a631651f36e86c8e24662e6e74146ef93b
Rodolfo Mayora <rodolfomayora96@gmail.com>
2021-03-29 16:38:21 -0400

    BLP-8: MainSection Mobile

--- pages/Home/index.tsx
+++ pages/Home/index.tsx
@@ -1,24 +1,38 @@
 import React, { FC } from 'react';
 import { Layout, Container, PrimaryButton } from '../../components';
-import styles from './style.module.scss';
+import style from './style.module.scss';
 
 const Home: FC = () => (
   <Layout pageTitle='Home'>
-    <main className={styles.Home}>
-      
-      <Container>
-        <section>
-          <h1>A modern publishing platform</h1>
-          <p>Grow your audience and build your online brand</p>
-          <div>
+    <main className={style.Home}>
+
+      {/* Main Section */}
+      <section className={style.mainSection}>
+        <Container>
+          <h1 className={style.mainTitle}>
+            A modern publishing platform
+          </h1>
+          <p className={style.mainParagraph}>
+            Grow your audience and build your online brand
+          </p>
+          <div className={style.mainButtons}>
             <PrimaryButton>Start for Free</PrimaryButton>
             <PrimaryButton buttonStyle='transparent'>
               Learn More
             </PrimaryButton>
           </div>
-        </section>
+        </Container>
+        <img className={style.background}
+          src="/images/svg/bg-pattern-intro.svg"
+          alt="background"
+        />
+      </section>
+      
+      <section>
+      </section>
+        
 
-      </Container>
+      
     </main>
   </Layout>
 );
