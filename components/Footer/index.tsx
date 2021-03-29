a341898e0516ccfe63f5baa452489c19402786a1
Rodolfo Mayora <rodolfomayora96@gmail.com>
2021-03-29 12:06:39 -0400

    BLP-7: Footer Desktop

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,27 +1,36 @@
 import React, { FC } from 'react';
 import style from './style.module.scss';
+import Conteiner from '../Container';
 import ProductLinks from '../ProductLinks';
 import CompanyLinks from '../CompanyLinks';
 import ConnectLinks from '../ConnectLinks';
 
 const Footer: FC = () => (
   <footer className={style.Footer}>
-    <img className={style.logo}
-      src="/images/svg/logo.svg"
-      alt="Blogr Logo"
-    />
-    <nav className={style.navLinks}>
-      <div className={style.categoryLinks}>Product</div>
-      <ProductLinks />
-    </nav>
-    <nav className={style.navLinks}>
-      <div className={style.categoryLinks}>Company</div>
-      <CompanyLinks />
-    </nav>
-    <nav className={style.navLinks}>
-      <div className={style.categoryLinks}>Connect</div>
-      <ConnectLinks />
-    </nav>
+    <Conteiner>
+      <img className={style.logo}
+        src="/images/svg/logo.svg"
+        alt="Blogr Logo"
+      />
+      <div className={style.linksWrapper}>
+        <div className={style.titleLinks}>Product</div>
+        <nav className={style.linkList}>
+          <ProductLinks />
+        </nav>
+      </div>
+      <div className={style.linksWrapper}>
+        <div className={style.titleLinks}>Company</div>
+        <nav className={style.linkList}>
+          <CompanyLinks />
+        </nav>
+      </div>
+      <div className={style.linksWrapper}>
+        <div className={style.titleLinks}>Connect</div>
+        <nav className={style.linkList}>
+          <ConnectLinks />
+        </nav>
+      </div>
+    </Conteiner>
   </footer>
 );
 
