03b039e18167a015327d1fd1727d7df625f1ff89
Rodolfo Mayora <rodolfomayora96@gmail.com>
2021-03-29 11:16:21 -0400

    BLP-6: Footer Mobile

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,8 +1,27 @@
 import React, { FC } from 'react';
+import style from './style.module.scss';
+import ProductLinks from '../ProductLinks';
+import CompanyLinks from '../CompanyLinks';
+import ConnectLinks from '../ConnectLinks';
 
 const Footer: FC = () => (
-  <footer>
-    Footer
+  <footer className={style.Footer}>
+    <img className={style.logo}
+      src="/images/svg/logo.svg"
+      alt="Blogr Logo"
+    />
+    <nav className={style.navLinks}>
+      <div className={style.categoryLinks}>Product</div>
+      <ProductLinks />
+    </nav>
+    <nav className={style.navLinks}>
+      <div className={style.categoryLinks}>Company</div>
+      <CompanyLinks />
+    </nav>
+    <nav className={style.navLinks}>
+      <div className={style.categoryLinks}>Connect</div>
+      <ConnectLinks />
+    </nav>
   </footer>
 );
 
