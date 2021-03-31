85a284a48f1560c7ec53f5cbc73403124defd251
Rodolfo Mayora <46007470+rodolfomayora@users.noreply.github.com>
2021-03-31 14:39:09 -0400

    Develop (#3)
    * BLP-1: Add Assets and Styles
    
    * BLP-2: Create Layout and Container
    
    * BLP-3: PrimaryButton
    
    * BLP-4: add Modal
    
    * BLP-4: MenuMobile
    
    * BLP-5: HeaderDesktop
    
    * BLP-6: Footer Mobile
    
    * BLP-7: Footer Desktop
    
    * BLP-8: MainSection Mobile
    
    * BLP-9: MainSection Desktop
    
    * ELP-10: MainSection Desktop
    
    * BLP-11: BannerSection responsive
    
    * BLP-13: Feature Section 2 Mobile
    
    * BLP-14: Feature Section 2 Desktop
    
    * BLP-15: add AtributionBlock
    
    * BLP-16: add test inner pages
    
    * BLP-17: Export to Static HTML and Deploy
    
    * hotfix: color browser navigation
    
    * hotfix: color
    
    * BLP-18: add Custom Document

---
+++ components/SplashScreen/index.tsx
@@ -0,0 +1,34 @@
+import React, { FC, useState, useEffect } from 'react';
+import style from './style.module.scss';
+
+const SplashScreen: FC = () => {
+  const visible: string = style.SplashScreen;
+  const hidden: string = style.SplashScreen + ' ' + style.hidden;
+  const [componentStyle, setCompnentStyle] = useState<string>(visible);
+  const [isLoad, setIsLoad] = useState<boolean>(true);
+  useEffect(() => {
+
+    const init = () => {
+      window.setTimeout(() => {
+        setCompnentStyle(hidden)
+      }, 1000);
+
+      window.setTimeout(() => {
+        setIsLoad(false)
+      }, 2000);
+    }
+
+    window.addEventListener('load', init);
+  }, [])
+  
+  return isLoad ? (
+    <div className={componentStyle}>
+      <img className={style.logo}
+        src="/images/svg/logo.svg"
+        alt="blogr logo"  
+      />
+    </div>
+  ) : <></>
+}
+
+export default SplashScreen;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import { AppProps } from 'next/app';
 import '../styles/style.global.scss';
 
+import { SplashScreen } from '../components';
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
       <div id="modal"></div>
+      <SplashScreen />
     </>
   )
 }
