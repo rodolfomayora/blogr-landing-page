b759fa4b2ef85d84b7e914ad014260e9e99b90da
Rodolfo Mayora <rodolfomayora96@gmail.com>
2024-05-21 09:29:20 -0400

    feat(cosmponents): add button-link-outlined component

---
+++ src/components/ButtonLinkOutlined/index.tsx
@@ -0,0 +1,13 @@
+import type { ComponentProps } from 'react';
+import { ButtonLink } from '../ButtonLink';
+import styles from './styles.module.scss';
+
+type Props = ComponentProps<typeof ButtonLink>;
+
+export function ButtonLinkOutlined({ children, ...props }: Props) {
+  return (
+    <ButtonLink className={styles.ButtonLinkOutlined} {...props}>
+      {children}
+    </ButtonLink>
+  );
+}
